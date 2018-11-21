5402d2e4d5347088a1a909c3bc15a117ef4eea2e
Brian Louie <brianalouie@gmail.com>
2018-11-20 22:23:00 -0800

    working scrollbar with sticky bottom and some css

--- components/Messenger.js
+++ components/Messenger.js
@@ -7,11 +7,11 @@ import Message from './Message'
 
 const generateName = () => {
   const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min;
-	const firstName = ["Jack", "Steven", "Brian", "Marc", "Drew", "Stephanie", "Daniel", "James"];
-	const lastName = ['Li', "Chung", "Tiosejo", "Louie", "Curtis", "Sockwell", "Jiang"];
-	const name = firstName[getRandomInt(0, firstName.length)] + ' ' + lastName[getRandomInt(0, lastName.length)]
+	const firstName = ["Jack", "Steven", "Brian", "Marc", "Drew", "Stephanie", "Daniel", "James", "Spencer", "Caitlin", "Jackie", "Julius", "Patrick"];
+	const lastName = ['Li', "Chung", "Tiosejo", "Louie", "Curtis", "Sockwell", "Jiang", "Bykowsky", "Detro", "Chen", "Sea", "Doyle"];
+	const name = firstName[getRandomInt(0, firstName.length)] + ' ' + lastName[getRandomInt(0, lastName.length)];
 	return name;
-}
+};
 
 class Messenger extends React.Component {
 
@@ -32,7 +32,12 @@ class Messenger extends React.Component {
  //    objDiv.scrollTop = objDiv.scrollHeight;
 	// }
 
+
+
 	componentDidUpdate() {
+    // const objDiv = document.getElementById('chatview');
+    // objDiv.scrollTop = objDiv.scrollHeight;
+    this.scrollToBottom();
 		if (!this.state.messages.length && !this.state.updated) {
       this.setState({ messages: this.props.messages , updated: true});
 		}
@@ -41,6 +46,7 @@ class Messenger extends React.Component {
 	componentDidMount() {
 		this.socket = io('http://localhost:3000');
 		this.socket.on('message', this.handleMessage);
+		setTimeout(this.scrollToBottom, 100);
 	}
 
 	componentWillUnmount() {
@@ -52,16 +58,16 @@ class Messenger extends React.Component {
 	handleMessage = (message) => {
       this.setState(state => ({ messages: state.messages.concat(message) }));
 			this.props.addMessage(message.text, message.username, message.created_at);
-	}
+	};
 
 	handleSubmit = e => {
-		e.preventDefault()
+		e.preventDefault();
 
 		const message = {
 			created_at: (new Date()).getTime(),
 			username: this.state.username,
 			text: this.state.text,
-		}
+		};
 
 		this.socket.emit('message', message);
 
@@ -70,11 +76,15 @@ class Messenger extends React.Component {
 			text: '',
 			messages: this.state.messages.concat(message)
 		}))
-	}
+	};
+
+	scrollToBottom = () => {
+		this.el.scrollIntoView({ behavior: 'instant'});
+	};
 
 	handleChange = e => {
 		this.setState({username: e.target.value})
-	}
+	};
 
 	render() {
 		const sameUser = (msg, i, arr) => {
@@ -88,7 +98,7 @@ class Messenger extends React.Component {
 
         <input type="text" onChange={this.handleChange} placeholder={"enter username"}/>
 
-				<div className="mdl-card mdl-shadow--2dp">
+				<div className="mdl-card mdl-shadow--2dp" id="chatview" >
 				<ul>
 					{/*{this.props.messages.map((message, i, array) => (*/}
           {/*<Message key={i} message={message} username={this.state.username} firstMessage={sameUser(message, i, array)}/>*/}
@@ -96,26 +106,33 @@ class Messenger extends React.Component {
 					{this.state.messages.map((message, i, array) => (
             <Message key={i} message={message} username={this.state.username} firstMessage={sameUser(message, i, array)}/>
 					))}
+					<div ref={el => { this.el = el;}} />
 				</ul>
-				<form onSubmit={this.handleSubmit}>
-					<div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-						<input
-							type="text"
-							value={this.state.text}
-							onChange={e => this.setState({ text: e.target.value })}
-							className="mdl-textfield__input"
-							id="input"
-							placeholder={"Send a message"}
-						/>
-						<label className="mdl-textfield__label" htmlFor="input">
+          <form onSubmit={this.handleSubmit}>
+            <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+              <input
+                type="text"
+                value={this.state.text}
+                onChange={e => this.setState({ text: e.target.value })}
+                className="mdl-textfield__input"
+                id="message-input"
+                placeholder={"Send a message"}
+              />
+              <label className="mdl-textfield__label" htmlFor="message-input">
 
-						</label>
-					</div>
-				</form>
+              </label>
+            </div>
+          </form>
 				<style>{`
+						#message-input {
+						border-bottom: lightgray solid 1px;
+						border-top: lightgray solid 1px;
+						padding: 10px;
+						height: 20px;
+						}
 						form {
 							background: #fff;
-							padding: 10px;
+							padding: 0px 10px 0px 10px;
 						}
 						ul {
 							min-height: 100px;
@@ -123,6 +140,7 @@ class Messenger extends React.Component {
 							padding: 0;
 							text-align: left;
 							list-style: none;
+							overflow-y: scroll;
 						}
 						ul li {
 							padding: 3px;
@@ -132,9 +150,12 @@ class Messenger extends React.Component {
 							margin: auto;
 							transition: all .3s;
 							transform: translateY(100px);
+							min-height: 500px;
+							max-height: 500px;
 						}
 					`}</style>
 			</div>
+
 				</>
 		)
 	}
